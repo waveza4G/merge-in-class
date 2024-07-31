84164a0fbf9d39604185f51a30135249621b2417
waveza4G <wavezaaia@gmail.com>
2024-07-31 11:16:22 +0700

    midified reset() method

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -93,6 +93,8 @@ export default {
       },
 
       onReset () {
+        id.value = null
+        surname.value = null
         name.value = null
         age.value = null
         accept.value = false
